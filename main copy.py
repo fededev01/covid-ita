b92c5b9a5f70e4c4b13bafccfb94c75cf36343d6
Federico <45139706+fededev01@users.noreply.github.com>
2020-11-12 23:45:11 +0100

    Update main copy.py

--- main copy.py
+++ main copy.py
@@ -7,43 +7,37 @@ url = 'https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-json/dpc-c
 reponse = requests.get(url)
 dati = json.loads(reponse.content)
 
-conf = [] 
-tamp = []
-mor = []
-n_pos= []
+conf, tamp, mor, n_pos = [], [], [], [] 
 for i in dati:
   conf.append(i["totale_casi"])
   tamp.append(i["tamponi"])
   mor.append(i["deceduti"])
   n_pos.append(i["nuovi_positivi"])
 
-
+del conf[-1]
+del n_pos[-1]
 nuovi_morti = np.array([mor]) 
+del mor[-1]
 n_m = np.diff(nuovi_morti)
 nuovi_m = n_m.reshape(len(conf), 1)
 
 nuovi_tamponi = np.array([tamp]) 
+del tamp[-1]
 n_t = np.diff(nuovi_tamponi)
 nuovi_t= n_t.reshape(len(conf), 1)
 
 dates = pd.date_range('20200224', periods = len(mor))
 df = pd.DataFrame(index = dates)
-df['tamponi'] = tamp
-df['confermati'] = conf
-df['deceduti'] = mor
+df['tamponi tot'] = tamp
+df['casi tot'] = conf
+df['morti tot'] = mor
+df['nuovi positivi'] = n_pos
 df['nuovi morti'] = nuovi_m
-df['nuovi tamponi'] = nuovi_t  
-
-
+df['nuovi tamponi'] = nuovi_t 
+ 
 ter = []
 fir = np.array([n_pos])
-sec = np.divide(nuovi_t, n_pos)
+sec = np.divide(n_pos, nuovi_t)
 
-
-#llll = np.dot(media, 100)
-
-#ter = [i * 100 for i in n_pos]
-
-#perc = np.array([ter])
-df['prova'] = sec[0]
+df['rapp pos/tamp'] = sec[0]
 print(df)
